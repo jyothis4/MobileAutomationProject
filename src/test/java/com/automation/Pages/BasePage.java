996d050c9171e6cfc782a05fa119b7e97367dbf1
Abhay Krishnan M N(UST <abhaykrishnan333@gmail.com>
2024-08-22 15:22:43 +0530

    FrameWork Updated

--- src/test/java/com/automation/Pages/BasePage.java
+++ src/test/java/com/automation/Pages/BasePage.java
@@ -2,6 +2,7 @@ package com.automation.Pages;
 
 import com.automation.Utils.DriverManager;
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Pause;
 import org.openqa.selenium.interactions.PointerInput;
@@ -47,7 +48,7 @@ public class BasePage {
         }
     }
 
-    public void scroll(int startX, int startY, int endX, int endY) {
+    public void scrollOrSwipe(int startX, int startY, int endX, int endY) {
         PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
         Sequence sequence = new Sequence(finger1, 1)
                 .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
@@ -57,6 +58,36 @@ public class BasePage {
                 .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
 
         driver.perform(Collections.singletonList(sequence));
+    }
+
+    public void zoom(WebElement element) {
+        int startX = element.getLocation().getX() + element.getSize().getWidth() / 2;
+        int startY = element.getLocation().getY() + element.getSize().getHeight() / 2;
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+
+        Sequence sequence1 = new Sequence(finger1, 1)
+                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
+                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(finger1, Duration.ofSeconds(2)))
+                .addAction(finger1.createPointerMove(Duration.ofSeconds(1), PointerInput.Origin.viewport(), startX + 100, startY + 100))
+                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        Sequence sequence2 = new Sequence(finger2, 1)
+                .addAction(finger2.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
+                .addAction(finger2.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(finger2, Duration.ofSeconds(2)))
+                .addAction(finger2.createPointerMove(Duration.ofSeconds(1), PointerInput.Origin.viewport(), startX - 100, startY - 100))
+                .addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Arrays.asList(sequence1, sequence2));
+    }
+
+    public void scroll() {
+        Dimension dimension = driver.manage().window().getSize();
+        int width = dimension.getWidth();
+        int height = dimension.getHeight();
+        scrollOrSwipe(width / 2, height/2, width / 2, 0);
     }
 
     public boolean isPresents(WebElement element) {
