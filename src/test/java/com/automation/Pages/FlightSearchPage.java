0155d759b69196bcdfabec368e48f785d4f72bae
Abhay Krishnan M N(UST <abhaykrishnan333@gmail.com>
2024-08-23 14:55:24 +0530

    changes in flight scenario

--- src/test/java/com/automation/Pages/FlightSearchPage.java
+++ src/test/java/com/automation/Pages/FlightSearchPage.java
@@ -11,6 +11,18 @@ public class FlightSearchPage extends BasePage {
     @FindBy(xpath = "//android.view.View[@content-desc='Going to Button']")
     WebElement flyingToInput;
 
+    @FindBy(xpath = "//android.widget.TextView[@resource-id='TeamTypeaheadInputPlaceholder']")
+    WebElement flyingFromTextInput;
+
+    @FindBy(xpath = "//android.widget.TextView[@resource-id='TeamTypeaheadInputPlaceholder']")
+    WebElement flyingToTextInput;
+
+    @FindBy(xpath = "//android.view.ViewGroup[@resource-id='android:id/content']/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.Button")
+    WebElement fromInput;
+
+    @FindBy(xpath = "//android.view.ViewGroup[@resource-id='android:id/content']/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.Button")
+    WebElement toInput;
+
 
     public boolean isFlightSearchPageDisplayed() {
         return isPresent(flyingFromInput) && isPresent(flyingToInput);
@@ -18,11 +30,13 @@ public class FlightSearchPage extends BasePage {
 
     public void enterFromLocation(String fromLocation) {
         flyingFromInput.click();
-        flyingFromInput.sendKeys(fromLocation);
+        flyingFromTextInput.sendKeys(fromLocation);
+        fromInput.click();
     }
 
     public void enterToLocation(String toLocation) {
         flyingToInput.click();
-        flyingToInput.sendKeys(toLocation);
+        flyingToTextInput.sendKeys(toLocation);
+        toInput.click();
     }
 }
